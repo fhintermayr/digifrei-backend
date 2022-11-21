aab908a2b406f6e62ad19f4026cdcbf631ef44dc
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2022-11-21 15:59:51 +0100

    Initial commit

---
+++ src/main/java/de/icp/match/MatchApplication.java
@@ -0,0 +1,13 @@
+package de.icp.match;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchApplication.class, args);
+	}
+
+}
