e9ec271a342552edf5c603843bde9fdd054129eb
Fabian <97839471+fhintermayr@users.noreply.github.com>
2022-11-23 14:04:06 +0100

    Project configuration cleanup (#2)
    * build: removed websocket dependency
    
    As the websocket broker will run on its own port, I excluded the dependency in order to create a new project dealing solely with the websocket broker.
    I also removed the test profile in MatchApplicationTests as it won't crash without it anymore
    
    * build: extended application-dev.properties
    
    I have added the hibernate sql dialect and the ddl-auto setting to the application-dev.properties file. I have also added a reminder comment to application.properties

--- src/test/java/de/icp/match/MatchApplicationTests.java
+++ src/test/java/de/icp/match/MatchApplicationTests.java
@@ -2,10 +2,8 @@ package de.icp.match;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles({ "dev" })
 class MatchApplicationTests {
 
 	@Test
