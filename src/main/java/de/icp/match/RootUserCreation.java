53cd53db70ac713e5e84d30210d078b962343e44
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-05-10 11:38:30 +0200

    refactor: adapted RootUserCreation to the new Apprentice class

--- src/main/java/de/icp/match/RootUserCreation.java
+++ src/main/java/de/icp/match/RootUserCreation.java
@@ -1,51 +1,30 @@
 package de.icp.match;
 
-import de.icp.match.user.model.AccessRole;
-import de.icp.match.user.model.Gender;
+import de.icp.match.user.model.Apprentice;
 import de.icp.match.user.model.User;
 import de.icp.match.user.repository.UserRepository;
-import de.icp.match.user.service.UserCreationService;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDate;
 
 @Component
 @Profile("dev")
 public class RootUserCreation implements ApplicationRunner {
 
-    private final UserCreationService userCreationService;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public RootUserCreation(UserCreationService userCreationService, UserRepository userRepository) {
-        this.userCreationService = userCreationService;
+    public RootUserCreation(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public void run(ApplicationArguments args)  {
-
-            User rootUser = new User(
-                    "admin",
-                    "admin",
-                    "admin",
-                    "icp",
-                    LocalDate.now(),
-                    Gender.DIVERSE,
-                    null,
-                    "admin",
-                    "admin",
-                    "1.2.3",
-                    null,
-                    null,
-                    null,
-                    AccessRole.ADMINISTRATOR,
-                    1
-            );
-
-        if (!userRepository.existsByUsername(rootUser.getUsername())) userCreationService.register(rootUser);
+        User emp = new Apprentice("foo", "bar", "mail", passwordEncoder.encode("pass"), "foo");
+        userRepository.save(emp);
     }
 
 }
