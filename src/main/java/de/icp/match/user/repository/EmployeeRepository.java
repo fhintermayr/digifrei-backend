d2984c91270df41c134d585d63de47c7f54dbaf8
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-05-10 15:05:54 +0200

    refactor: removed unused Employee entity and its repository

--- src/main/java/de/icp/match/user/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package de.icp.match.user.repository;
-
-import de.icp.match.user.model.Employee;
-
-import java.util.Optional;
-
-public interface EmployeeRepository  {
-    Optional<Employee> findByEmail(String email);
-}
