6d47598b3f519ae1371f6c5f49d2b92773b87e21
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-05-11 20:25:41 +0200

    feat: created dto of ExemptionRequest and RequestProcessing with their corresponding mappers

---
+++ src/main/java/de/icp/match/request/dto/ExemptionRequestDto.java
@@ -0,0 +1,24 @@
+package de.icp.match.request.dto;
+
+import de.icp.match.request.model.ExemptionCategory;
+import de.icp.match.user.dto.ApprenticeDto;
+import jakarta.validation.constraints.FutureOrPresent;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * A DTO for the {@link de.icp.match.request.model.ExemptionRequest} entity
+ */
+public record ExemptionRequestDto(
+        UUID id,
+        @FutureOrPresent LocalDateTime startTime,
+        @FutureOrPresent LocalDateTime endTime,
+        LocalDateTime submissionDate,
+        String reason,
+        ExemptionCategory exemptionCategory,
+        ApprenticeDto applicant,
+        RequestProcessingDto requestProcessing
+) implements Serializable {
+}
---
+++ src/main/java/de/icp/match/request/mapper/ExemptionRequestMapper.java
@@ -0,0 +1,25 @@
+package de.icp.match.request.mapper;
+
+import de.icp.match.request.dto.ExemptionRequestDto;
+import de.icp.match.request.model.ExemptionRequest;
+import de.icp.match.request.model.RequestProcessing;
+import de.icp.match.user.mapper.UserMapper;
+import org.mapstruct.*;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING, uses = {UserMapper.class, RequestProcessingMapper.class})
+public interface ExemptionRequestMapper {
+    ExemptionRequest toEntity(ExemptionRequestDto exemptionRequestDto);
+
+    ExemptionRequestDto toDto(ExemptionRequest exemptionRequest);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    ExemptionRequest partialUpdate(ExemptionRequestDto exemptionRequestDto, @MappingTarget ExemptionRequest exemptionRequest);
+
+    @AfterMapping
+    default void linkRequestProcessing(@MappingTarget ExemptionRequest exemptionRequest) {
+        RequestProcessing requestProcessing = exemptionRequest.getRequestProcessing();
+        if (requestProcessing != null) {
+            requestProcessing.setExemptionRequest(exemptionRequest);
+        }
+    }
+}
