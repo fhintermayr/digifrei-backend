e2b46003dc0ff6a1217540492fd81f1e3833ec7a
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-05-11 16:13:41 +0200

    feat: added SocioEduExpert entity class and referenced it in the Apprentice entity

--- src/main/java/de/icp/match/user/model/Apprentice.java
+++ src/main/java/de/icp/match/user/model/Apprentice.java
@@ -1,24 +1,27 @@
 package de.icp.match.user.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
 public class Apprentice extends User {
 
-    private String doc;
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, optional = false)
+    @JoinColumn(name = "socio_edu_expert_id", nullable = false)
+    private SocioEduExpert socioEduExpert;
 
-
-    public Apprentice(String firstName, String lastName, String email, String password, Department department, String doc) {
+    public Apprentice(String firstName, String lastName, String email, String password, Department department, SocioEduExpert socioEduExpert) {
         super(firstName, lastName, email, password, department);
-        this.doc = doc;
+        this.socioEduExpert = socioEduExpert;
     }
 
-    public Apprentice() {
-
-    }
 }
