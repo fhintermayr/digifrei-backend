ee2da94d578a481d5722752ee5bcdad9bf462921
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-05-12 12:03:05 +0200

    feat: added ApprenticeUpdateDto and let it extend from UserUpdateDto

--- src/main/java/de/icp/match/user/dto/UserUpdateDto.java
+++ src/main/java/de/icp/match/user/dto/UserUpdateDto.java
@@ -1,7 +1,12 @@
 package de.icp.match.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import de.icp.match.user.dto.apprentice.ApprenticeUpdateDto;
 import de.icp.match.user.model.User;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.UUID;
@@ -10,9 +15,15 @@ import java.util.UUID;
  * A DTO for the {@link User} entity
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = ApprenticeUpdateDto.class)
+})
 public class UserUpdateDto implements Serializable {
-    private final String firstName;
-    private final String lastName;
-    private final String email;
-    private final UUID departmentId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private UUID departmentId;
 }
