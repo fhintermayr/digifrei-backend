c03f3ed884cc0acea7d30532d0b1189bd8fa36f9
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-05-12 12:22:50 +0200

    feat: added enum converter for ExemptionCategory

--- src/main/java/de/icp/match/request/model/ExemptionCategory.java
+++ src/main/java/de/icp/match/request/model/ExemptionCategory.java
@@ -1,7 +1,16 @@
 package de.icp.match.request.model;
 
+import lombok.Getter;
+
 public enum ExemptionCategory {
-    MEDICAL_APPOINTMENT,
-    OFFICE_APPOINTMENT,
-    OTHER
+    MEDICAL_APPOINTMENT(1),
+    OFFICE_APPOINTMENT(2),
+    OTHER(3);
+
+    @Getter
+    private final Integer code;
+
+    ExemptionCategory(Integer code) {
+        this.code = code;
+    }
 }
