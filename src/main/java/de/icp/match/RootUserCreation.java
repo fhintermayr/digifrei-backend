74b4adb78f412e40d087ad410dcd3166e52eddf1
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-05-12 12:17:44 +0200

    test: adapted RootUserCreation to new data model

--- src/main/java/de/icp/match/RootUserCreation.java
+++ src/main/java/de/icp/match/RootUserCreation.java
@@ -1,5 +1,8 @@
 package de.icp.match;
 
+import de.icp.match.request.model.ExemptionCategory;
+import de.icp.match.request.model.ExemptionRequest;
+import de.icp.match.request.repository.ExemptionRequestRepository;
 import de.icp.match.user.model.*;
 import de.icp.match.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
@@ -9,16 +12,21 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
 @Profile("dev")
 public class RootUserCreation implements ApplicationRunner {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final ExemptionRequestRepository exemptionRequestRepository;
 
-    public RootUserCreation(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public RootUserCreation(UserRepository userRepository, PasswordEncoder passwordEncoder,
+                            ExemptionRequestRepository exemptionRequestRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.exemptionRequestRepository = exemptionRequestRepository;
     }
 
     @Override
@@ -28,11 +36,22 @@ public class RootUserCreation implements ApplicationRunner {
         Department dep2 = new Department("foo");
         SocioEduExpert see = new SocioEduExpert("hans", "wurst", "foo@bar.com");
 
-        User emp = new Apprentice( "bar", "mail", "mail", passwordEncoder.encode("pass"), dep,see);
-        User trainer = new Trainer( "bar", "mail", "mail2", passwordEncoder.encode("pass"), dep2);
+        Apprentice emp = new Apprentice( "bar", "mail", "mail", passwordEncoder.encode("pass"), dep,see);
+        Trainer trainer = new Trainer( "bar", "mail", "mail2", passwordEncoder.encode("pass"), dep2);
 
         if (!userRepository.existsByEmail(emp.getEmail())) userRepository.save(emp);
         if (!userRepository.existsByEmail(trainer.getEmail())) userRepository.save(trainer);
+
+
+        ExemptionRequest exemptionRequest = new ExemptionRequest(
+                LocalDateTime.now().plusDays(1),
+                LocalDateTime.now().plusDays(1),
+                "irgendwas",
+                ExemptionCategory.MEDICAL_APPOINTMENT,
+                emp
+        );
+
+        exemptionRequestRepository.save(exemptionRequest);
     }
 
 }
