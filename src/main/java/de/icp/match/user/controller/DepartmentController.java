7dba0b11221571a88ec06057a42656d519e1d5ab
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-05-14 13:29:17 +0200

    feat(department): endpoint for querying all departments

---
+++ src/main/java/de/icp/match/user/controller/DepartmentController.java
@@ -0,0 +1,27 @@
+package de.icp.match.user.controller;
+
+import de.icp.match.user.model.Department;
+import de.icp.match.user.service.DepartmentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+    @GetMapping("department")
+    public ResponseEntity<List<Department>> getAllDepartments() {
+
+        List<Department> allDepartments = departmentService.loadAllDepartments();
+
+        return ResponseEntity.ok(allDepartments);
+    }
+}
