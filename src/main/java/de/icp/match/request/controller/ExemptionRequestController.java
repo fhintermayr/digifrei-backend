1fa9300ce6783a66b0ae8b1e910e2b3af118106f
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-05-15 12:39:47 +0200

    fix: added missing cors header

--- src/main/java/de/icp/match/request/controller/ExemptionRequestController.java
+++ src/main/java/de/icp/match/request/controller/ExemptionRequestController.java
@@ -8,10 +8,12 @@ import de.icp.match.request.service.SubmitRequestService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class ExemptionRequestController {
 
