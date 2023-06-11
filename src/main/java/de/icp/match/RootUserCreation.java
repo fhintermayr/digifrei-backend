e3864e8f404b11220807a7db5b2969d1db625bde
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-06-11 16:34:59 +0200

    refactor: adapted sample data in RootUserCreation class

--- src/main/java/de/icp/match/RootUserCreation.java
+++ src/main/java/de/icp/match/RootUserCreation.java
@@ -2,7 +2,10 @@ package de.icp.match;
 
 import de.icp.match.request.model.ExemptionCategory;
 import de.icp.match.request.model.ExemptionRequest;
+import de.icp.match.request.model.ProcessingStatus;
+import de.icp.match.request.model.RequestProcessing;
 import de.icp.match.request.repository.ExemptionRequestRepository;
+import de.icp.match.request.repository.RequestProcessingRepository;
 import de.icp.match.user.model.*;
 import de.icp.match.user.repository.SocioEduExpertRepository;
 import de.icp.match.user.repository.UserRepository;
@@ -23,26 +26,28 @@ public class RootUserCreation implements ApplicationRunner {
     private final PasswordEncoder passwordEncoder;
     private final ExemptionRequestRepository exemptionRequestRepository;
     private final SocioEduExpertRepository socioEduExpertRepository;
+    private final RequestProcessingRepository requestProcessingRepository;
 
     public RootUserCreation(UserRepository userRepository, PasswordEncoder passwordEncoder,
-                            ExemptionRequestRepository exemptionRequestRepository, SocioEduExpertRepository socioEduExpertRepository) {
+                            ExemptionRequestRepository exemptionRequestRepository, SocioEduExpertRepository socioEduExpertRepository,
+                            RequestProcessingRepository requestProcessingRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.exemptionRequestRepository = exemptionRequestRepository;
         this.socioEduExpertRepository = socioEduExpertRepository;
+        this.requestProcessingRepository = requestProcessingRepository;
     }
 
     @Override
     @Transactional
     public void run(ApplicationArguments args)  {
         Department dep = new Department("dep");
-        Department dep2 = new Department("foo");
-        SocioEduExpert see = new SocioEduExpert("hans", "wurst", "foo@bar.com");
+        SocioEduExpert see = new SocioEduExpert("Frau", "Fachdienst", "fachidenst@icp.de");
 
         socioEduExpertRepository.save(see);
 
-        Apprentice emp = new Apprentice( "bar", "mail", "mail", passwordEncoder.encode("pass"), dep,see);
-        Trainer trainer = new Trainer( "bar", "mail", "mail2", passwordEncoder.encode("pass"), dep2);
+        Apprentice emp = new Apprentice( "Mr", "Madl", "mr@madl.de", passwordEncoder.encode("pass"), dep,see);
+        Trainer trainer = new Trainer( "Herr", "Ausbilder", "mail2", passwordEncoder.encode("pass"), dep);
 
         if (!userRepository.existsByEmail(emp.getEmail())) userRepository.save(emp);
         if (!userRepository.existsByEmail(trainer.getEmail())) userRepository.save(trainer);
@@ -56,7 +61,22 @@ public class RootUserCreation implements ApplicationRunner {
                 emp
         );
 
+
+        ExemptionRequest exemptionRequest2 = new ExemptionRequest(
+                LocalDateTime.now().plusDays(1),
+                LocalDateTime.now().plusDays(1),
+                "irgendwas anderes",
+                ExemptionCategory.OFFICE_APPOINTMENT,
+                emp
+        );
+
         exemptionRequestRepository.save(exemptionRequest);
+        exemptionRequestRepository.save(exemptionRequest2);
+
+        RequestProcessing rp = new RequestProcessing(exemptionRequest, ProcessingStatus.APPROVED, "komm ja nicht wieder", trainer);
+
+        requestProcessingRepository.save(rp);
+
     }
 
 }
