8676161c5a27dd34627864d30d8c30776e82a204
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-06-18 17:21:30 +0200

    feat: service and controller for creating department

--- src/main/java/de/icp/match/user/service/DepartmentService.java
+++ src/main/java/de/icp/match/user/service/DepartmentService.java
@@ -1,5 +1,6 @@
 package de.icp.match.user.service;
 
+import de.icp.match.user.exception.DepartmentCreationException;
 import de.icp.match.user.model.Department;
 import de.icp.match.user.repository.DepartmentRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -15,6 +16,18 @@ public class DepartmentService {
 
     public DepartmentService(DepartmentRepository departmentRepository) {
         this.departmentRepository = departmentRepository;
+    }
+
+    public Department createDepartment(String departmentName) {
+
+        Department departmentToCreate = new Department(departmentName);
+
+        try {
+            return departmentRepository.save(departmentToCreate);
+        }
+        catch (Exception e) {
+            throw new DepartmentCreationException("Fehler bei der Erstellung der Abteilung", e);
+        }
     }
 
     public Department findById(Long departmentId) {
